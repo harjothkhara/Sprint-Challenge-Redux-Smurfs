bc50462a987f75c3dba31bb62dc350bb41fb7e1a
harjothkhara <harjoth.khara@gmail.com>
2019-05-17 08:52:47 -0700

    components: smurfs, smurf added, imported browserouter, route, smurfs to app.js

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Smurf.css';
+
+class Smurf extends Component {
+    render() {
+        return (
+            <h2>placeholder from smurf</h2>
+        )
+    }
+}
